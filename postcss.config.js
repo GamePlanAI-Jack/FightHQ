faf91b34747b36ae15cea5806fc701aec7bee267
GamePlanAI-Jack <jack.brindle310@gmail.com>
2025-05-18 23:34:09 +1000

    Fix PostCSS configuration for Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    function () {
-      console.log("PostCSS config loaded!");
-    }
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 };
